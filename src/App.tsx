ef1258dda886921d970b6f50b574cb3516daf632
Avtorre <113251940+Avtorre@users.noreply.github.com>
2024-10-02 21:59:20 +0300

    removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import './styles/App.css';
-import Header from './components/Header/Header';
-import Main from './pages/Main/Main';
-import Footer from './components/UI/Footer/Footer';
-
+import React from "react";
+import "./styles/App.css";
+import Header from "./components/Header/Header";
+import Main from "./pages/Main/Main";
+import Footer from "./components/UI/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-        <Main/>
-      <Footer/>
+      <Header />
+      <Main />
     </div>
   );
 }
-
+// <Footer/>
 export default App;
